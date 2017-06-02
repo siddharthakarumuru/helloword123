d08e5b18be35b018843d2f5285deb115d05dc764
siddharthakarumuru <sidch95@gmail.com>
2017-06-02 15:03:49 +0530

    Update hello2.java

--- hello2.java
+++ hello2.java
@@ -6,5 +6,6 @@ System.out.println("This is hello2 class");
   System.out.println("This is a new line");
  System.out.println("This is line 4");
 System.out.println("This is line 5");
+  System.out.println("This is line 6");
 }
 }
